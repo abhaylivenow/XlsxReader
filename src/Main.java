5a9c703327d88113b89d5e130a0eb0f85d29e6ca
abhaymaurya <abhay10june1999@gmail.com>
2022-09-14 18:54:17 +0530

    GUI ready with functionality.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,52 @@
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("/Users/abhaymaurya/Downloads/string.xlsx");
+
+        JFrame frame = new JFrame("Excel to XML converter");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000,200);
+
+        JPanel panel = new JPanel();
+        JLabel labelSource = new JLabel("Source Location");
+        JTextField edtSourceLocation = new JTextField(20);
+        JLabel labelDestination = new JLabel("Destination location");
+        JTextField edtDestinationLocation = new JTextField(20);
+        JButton btn = new JButton("Generate xml files");
+        panel.add(labelSource);
+        panel.add(edtSourceLocation);
+        panel.add(labelDestination);
+        panel.add(edtDestinationLocation);
+        panel.add(edtDestinationLocation);
+        panel.add(btn);
+
+        frame.getContentPane().add(panel);
+
+        frame.setVisible(true);
+
+        btn.addActionListener(e -> {
+            System.out.println(edtSourceLocation.getText());
+            System.out.println(edtDestinationLocation.getText());
+            try {
+                init(edtSourceLocation.getText(),edtDestinationLocation.getText());
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+    }
+
+    private static void init(String sourceLocation, String destinationLocation) throws IOException {
+        File file = new File(sourceLocation);
         FileInputStream fis = new FileInputStream(file);
         XSSFWorkbook wb = new XSSFWorkbook(fis);
         XSSFSheet sheet = wb.getSheetAt(0);
@@ -35,19 +70,19 @@ public class Main {
             }
         }
 
-        generateFilesAndFolders(outputList);
+        generateFilesAndFolders(outputList, destinationLocation);
     }
 
-    private static void generateFilesAndFolders(List<String> outputList) throws IOException {
-        File f1 = new File("/Users/abhaymaurya/Downloads/GeneratedFolders");
+    private static void generateFilesAndFolders(List<String> outputList, String destinationLocation) throws IOException {
+        File f1 = new File(destinationLocation+"GeneratedFolders");
         f1.mkdir();
-        createEnglishFile(outputList);
-        createHindiFile(outputList);
-        createHinglishFile(outputList);
+        createEnglishFile(outputList, destinationLocation);
+        createHindiFile(outputList, destinationLocation);
+        createHinglishFile(outputList, destinationLocation);
     }
 
-    private static void createEnglishFile(List<String> outputList) throws IOException {
-        File file = new File("/Users/abhaymaurya/Downloads/GeneratedFolders/EnglishFile.txt");
+    private static void createEnglishFile(List<String> outputList, String destinationLocation) throws IOException {
+        File file = new File(destinationLocation+"GeneratedFolders/EnglishFile.txt");
         boolean result;
         try {
             result = file.createNewFile();
@@ -60,7 +95,7 @@ public class Main {
             e.printStackTrace();
         }
 
-        FileWriter writer = new FileWriter("/Users/abhaymaurya/Downloads/GeneratedFolders/EnglishFile.txt");
+        FileWriter writer = new FileWriter(destinationLocation+"GeneratedFolders/EnglishFile.txt");
 
         try {
             for (int i = 0; i < outputList.size() - 1; i += 4) {
@@ -73,8 +108,8 @@ public class Main {
         }
     }
 
-    private static void createHinglishFile(List<String> outputList) throws IOException {
-        File file = new File("/Users/abhaymaurya/Downloads/GeneratedFolders/HinglishFile.txt");
+    private static void createHinglishFile(List<String> outputList, String destinationFolder) throws IOException {
+        File file = new File(destinationFolder+"GeneratedFolders/HinglishFile.txt");
         boolean result;
         try {
             result = file.createNewFile();
@@ -87,7 +122,7 @@ public class Main {
             e.printStackTrace();
         }
 
-        FileWriter writer = new FileWriter("/Users/abhaymaurya/Downloads/GeneratedFolders/HinglishFile.txt");
+        FileWriter writer = new FileWriter(destinationFolder+"GeneratedFolders/HinglishFile.txt");
 
         try {
             for (int i = 0; i < outputList.size() - 1; i += 4) {
@@ -100,8 +135,8 @@ public class Main {
         }
     }
 
-    private static void createHindiFile(List<String> outputList) throws IOException {
-        File file = new File("/Users/abhaymaurya/Downloads/GeneratedFolders/HindiFile.txt");
+    private static void createHindiFile(List<String> outputList, String destinationFolder) throws IOException {
+        File file = new File(destinationFolder+"GeneratedFolders/HindiFile.txt");
         boolean result;
         try {
             result = file.createNewFile();
@@ -114,7 +149,7 @@ public class Main {
             e.printStackTrace();
         }
 
-        FileWriter writer = new FileWriter("/Users/abhaymaurya/Downloads/GeneratedFolders/HindiFile.txt");
+        FileWriter writer = new FileWriter(destinationFolder+"GeneratedFolders/HindiFile.txt");
 
         try {
             for (int i = 0; i < outputList.size() - 1; i += 4) {
